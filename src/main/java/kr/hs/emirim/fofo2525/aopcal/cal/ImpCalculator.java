934e94be66d8224c224ba46dcfa6c7dc8a602d7e
Seungah Kim <fofo2525@naver.com>
2018-06-20 14:29:37 +0900

    실행시간 측정하기 2 - 메서드 실행 전후 시간 측정

--- src/main/java/kr/hs/emirim/fofo2525/aopcal/cal/ImpCalculator.java
+++ src/main/java/kr/hs/emirim/fofo2525/aopcal/cal/ImpCalculator.java
@@ -1,16 +1,15 @@
 package kr.hs.emirim.fofo2525.aopcal.cal;
 
-public class ImpCalculator implements Calculator{
+public class ImpCalculator implements Calculator {
 	@Override
 	public long factorial(long num) {
-		long start = System.currentTimeMillis();
+
 		long result = 1;
 		for (long i = 1; i <= num; i++) {
 			result *= i;
 		}
-		long end = System.currentTimeMillis();
-		System.out.printf("Imp factorial(%d): 실행시간 = %d", num, (end - start));
+
 		return result;
 	}
-	
+
 }
